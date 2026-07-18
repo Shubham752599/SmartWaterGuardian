a6c76b2b70db95c4512320ba311cf7d0df11ff2e
Shubham Maurya <YOUR_GITHUB_EMAIL>
2026-07-18 22:18:33 +0530

    Move templates and static into backend

--- backend/app.py
+++ backend/app.py
@@ -25,13 +25,15 @@ app = Flask(
 UPLOAD_FOLDER = os.path.join(BASE_DIR, "static", "uploads")
 app.config["UPLOAD_FOLDER"] = UPLOAD_FOLDER
 
-app.config['MYSQL_HOST'] = 'mysql-5336d3f-shubhamkushwaha7525-4c26.i.aivencloud.com'
-app.config['MYSQL_PORT'] = 15992
-app.config['MYSQL_USER'] = 'avnadmin'
+app.config['MYSQL_HOST'] = os.getenv("MYSQL_HOST")
+app.config['MYSQL_USER'] = os.getenv("MYSQL_USER")
 app.config['MYSQL_PASSWORD'] = os.getenv("MYSQL_PASSWORD")
-app.config['MYSQL_DB'] = 'smart_water_guardian'
+app.config['MYSQL_DB'] = os.getenv("MYSQL_DB")
+app.config['MYSQL_PORT'] = int(os.getenv("MYSQL_PORT"))'
 app.config['MYSQL_SSL_CA'] = 'ca.pem'
 app.secret_key = "smartwaterguardian123"
+app.config["MYSQL_SSL_MODE"] = "REQUIRED"
+app.config["MYSQL_SSL_CA"] = "ca.pem"
 
 mysql = MySQL(app)
 
